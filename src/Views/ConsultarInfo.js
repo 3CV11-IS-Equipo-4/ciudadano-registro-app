9624d2b55825a9787f62fc6c36ba812bd4a581d0
Poncho1409 <lhernandezm1409@alumno.ipn.mx>
2021-06-04 20:30:47 -0500

    Funcionalidad de consultar información persona fisica

--- src/Views/ConsultarInfo.js
+++ src/Views/ConsultarInfo.js
@@ -2,27 +2,44 @@ import Layout from '../Components/Layout';
 import RowData from '../Components/RowData';
 import Table from '../Components/Table';
 import {tables} from '../utils/const';
+import axios from 'axios';
+import protect from '../utils/protect';
+import payload from '../utils/payload';
+import {useEffect, useState} from 'react';
+import { useHistory } from "react-router-dom";
 
-export default function ConsultarInfo(){
+
+function ConsultarInfo(){
+    const history = useHistory();
+    const path= 'https://cdmx-registro-ciudadano-api.herokuapp.com/';
+    
+    const [data, setData] = useState(null);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const token = window.localStorage.getItem('token')
+        const p = payload();
+        const config = {
+            headers:{
+               Authorization: `Bearer ${token}` 
+            }
+        };
+        console.log("p",p);
+        axios.get(path+'physical_persons/'+p.sub._id,config)
+            .then(({data, status})=>{
+                if(status === 200) {
+                    setData(data);
+                    setUser(p);
+                    console.log("data",data);
+                }
+            }).catch(error => {
+                console.log("error",error);
+            });
+    }, []);
+
     // Variable de prueba
-    let type = "persona-moral"
-    let data = {
-        facha_nacido: '21/06/1999',
-        curp: '21/06/1999',
-        rfc: '21/06/1999',
-        nombre: '21/06/1999',
-        primer_apellido: '21/06/1999',
-        segundo_apellido: '21/06/1999',
-        calle: '21/06/1999',
-        num_exterior: '21/06/1999',
-        num_interior: '21/06/1999',
-        cp: '21/06/1999',
-        alcaldia: '21/06/1999',
-        colonia: '21/06/1999',
-        email: '21/06/1999',
-        facebook: '21/06/1999',
-        google: '21/06/1999'
-    };
+    let type = "ciudadano"
+    
     let data_personas = [];
 
     const aceptar = () => {};
@@ -32,38 +49,18 @@ export default function ConsultarInfo(){
     return (
         <Layout type={type}>
             <div className="container  text-start">
+                {user ? 
                 <div className="mb-4 mt-3">
-                    <h3 className="mb-3">Datos personales</h3>
-
-                    {type === "ciudadano" ? 
-                    <>
-                    <RowData label1="Fecha de nacimiento:" data1={data.facha_nacido} label2="CURP:" data2={data.curp} label3="RFC:" data3={data.rfc}/>
-                    <RowData label1="Nombre(s):" data1={data.nombre} label2="Primer apellido:" data2={data.primer_apellido} label3="Segundo apellido:" data3={data.segundo_apellido} />
-                    </>
-                    :<></>}
-                    {type === "persona-moral" ? 
-                    <RowData label1="RFC:" data1={data.rfc} label2="Denominación social:" data2={data.denominacion} label3="Razón social:" data3={data.razon} />
-                    :<></>}
-                    <RowData label1="Calle:" data1={data.calle} label2="Número exterior:" data2={data.num_exterior} label3="Número interior:" data3={data.num_interior} />
-                    <RowData label1="C.P.:" data1={data.cp} label2="Alcaldia:" data2={data.alcaldia} label3="Colonia:" data3={data.colonia} />
-                    
-                    <h3 className="mb-3">Datos de la cuenta</h3>
-                    {type === "ciudadano" ?
-                    <RowData label1="Correo electrónico:" data1={data.email} label2="Asosiado con facebook:" data2={data.facebook} label3="Asosiado con google:" data3={data.google} />
-                    :<></>}
-                    {type === "persona-moral" ?
-                    <>
-                    <RowData label1="Correo electrónico:" data1={data.email} label2="RFC del responsable:" data2={data.rfc_resp} label3="CURP del responsable:" data3={data.curp_resp} />
-                    <h3 className="mb-3">Personas asociadas</h3>
-                    {data_personas ? 
-                    <Table cols={tables.consultarInfo} datos={data_personas} aceptar={aceptar} denegar={denegar}></Table>
-                    :'No hay personas asociadas'}
-                    </>
-                    :<></>}
-                    
-                    
-                </div>
+                <h3 className="mb-3">Datos personales</h3>
+                <RowData label1="Fecha de nacimiento:" data1={data.birth_date} label2="CURP:" data2="hola" label3="RFC:" data3="hola"/>
+                <RowData label1="Nombre(s):" data1={data.names} label2="Primer apellido:" data2={data.first_surname} label3="Segundo apellido:" data3={data.second_surname} />
+                <RowData label1="Calle:" data1={data.addresses[data.actual_address].street} label2="Número exterior:" data2={data.addresses[data.actual_address].external_number} label3="Número interior:" data3={data.addresses[data.actual_address].internal_number} />
+                <RowData label1="C.P.:" data1={data.addresses[data.actual_address].postal_code} label2="Alcaldia:" data2={data.addresses[data.actual_address].town} label3="Colonia:" data3={data.addresses[data.actual_address].suburb} />
+            </div>:
+            <></>}
             </div>
         </Layout>
     );
 }
+
+export default protect(ConsultarInfo)
